2920e933c6817612073f7510085918fdaa05befe
Tyler Hunt <tyhunt04@gmail.com>
2019-05-01 02:01:29 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20190501020114) do
+
+  create_table "create_rides_passengers", force: :cascade do |t|
+    t.integer  "ride_id"
+    t.integer  "passenger_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
